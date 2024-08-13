000e96c0e7791b367f5c1cb65c1f0a5bad4eb22f
Glitjch <73555664+glitjch@users.noreply.github.com>
2024-08-13 13:06:37 -0700

    Added test scripts in package.json

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { chromium } = require('playwright');
 async function sortHackerNewsArticles() {
   // Delay used with  nodemon, so browser isn't popping up frequently.
 	const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-	console.log('Waiting for 10 seconds...');
-	await delay(10000);
+	console.log('Waiting for 3 seconds...');
+	await delay(3000);
 	console.log('Refreshing...');
 
 	// launch browser
